ab5e37922557b8846ff8477854e87834d4d4e8e8
guilhermewebdev <guilherme@guilhermeweb.dev>
2020-11-17 20:06:15 -0300

    Configuração do ambiente

--- api/settings.py
+++ api/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(lm6140pmyts!2eoo17&$k*!$06@ipeczng7@n7&(9sqvh&x_a'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
