6344ef344bc0dc47592f68d34085e993dbd20eb8
guilherme@localhost <guilherme@localhost>
2020-11-19 02:00:09 -0300

    Testes em banco de dados local

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import User, Professional, Rating
+class TestUser(TestCase):
 
-# Create your tests here.
+    def test_creation(self):
+        user = User(
+            email='teste@teste.com',
+            full_name='Guido Rossum',
+            celphone='32999198822',
+            password='tetris2',
+        )
+        user.save()
+    
