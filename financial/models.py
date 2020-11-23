44d265f497e6b0a3237fe9c7c05daa2217b9255c
guilherme@localhost <guilherme@localhost>
2020-11-23 09:15:31 -0300

    Criação do modelo dos pagamentos

--- financial/models.py
+++ financial/models.py
@@ -1,3 +1,37 @@
+from django.core.exceptions import ValidationError
+from services.models import Job
+from core.models import Professional
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+class Payment(models.Model):
+    client = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE,
+        related_name='payments',
+    )
+    professional = models.ForeignKey(
+        Professional,
+        on_delete=models.SET_NULL,
+        related_name='receipts',
+        null=True,
+    )
+    value = models.FloatField()
+    job = models.OneToOneField(
+        Job,
+        on_delete=models.PROTECT
+    )
+    registration_date = models.DateTimeField(
+        auto_now=True,
+    )
+
+    def validate_value(self):
+        if self.value != self.job.value:
+            raise ValidationError(
+                'Incorrect payment amount'
+            )
+    
+    def full_clean(self, *args, **kwargs):
+        self.validate_value()
+        return super(Payment, self).full_clean(*args, **kwargs)
--- financial/tests.py
+++ financial/tests.py
@@ -1,3 +1,71 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
+from .models import Payment
+from django.contrib.auth import get_user_model
+from core.models import Professional
+from services.models import Job, Proposal
+from django.utils import timezone
 
-# Create your tests here.
+User = get_user_model()
+TODAY = timezone.now()
+timedelta = timezone.timedelta
+class TestPayment(TestCase):
+
+    def setUp(self):
+        self.client = User(
+            email='tete@tete.com',
+            password='senha',
+            full_name='Fulano de tal',
+        )
+        self.client.save()
+        self.user = User(
+            email='tet2e@tete.com',
+            password='senha',
+            full_name='Fulano de tal',
+        )
+        self.user.save()
+        self.professional = Professional(
+            user=self.user,
+            state='MG',
+            city='Belo Horizonte',
+            address='Centro',
+            zip_code='36200-000',
+            cpf="529.982.247-25",
+            rg='mg3434032',
+            skills=['CI', 'AE', 'EM'],
+            occupation='CI',
+            avg_price=80,
+            coren='10.000'
+        )
+        self.professional.save()
+        self.proposal = Proposal(
+            client=self.client,
+            professional=self.professional,
+            city='Curitiba',
+            state='PR',
+            professional_type='AE',
+            service_type='AC',
+            start_datetime=TODAY + timedelta(days=1),
+            end_datetime=TODAY + timedelta(days=3),
+            value=300.00,
+            description='Lorem Ipsum dolores'
+        )
+        self.proposal.save()
+        self.proposal.accept()
+        self.job = self.proposal.job
+        self.payment = Payment(
+            client=self.client,
+            professional=self.professional,
+            value=300,
+            job=self.job
+        )
+        self.payment.full_clean()
+        self.payment.save()
+    
+    def test_payment(self):
+        self.payment.full_clean()
+
+    def test_wrong_payment_value(self):
+        self.payment.value = 100
+        self.assertRaises(ValidationError, self.payment.full_clean)
+        
