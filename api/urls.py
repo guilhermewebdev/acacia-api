be77bcfc61bf3a257f2399c1f72ece8bd00902f6
guilhermewebdev <guilherme@guilhermeweb.dev>
2020-11-25 11:54:52 -0300

    Carregamento de arquivos estáticos

--- api/urls.py
+++ api/urls.py
@@ -20,10 +20,12 @@ from core.views import professional_postback
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
 from graphql_jwt.decorators import jwt_cookie
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('postback/payment/<uuid:str>/', payment_postback),
     path('postback/professional/<uuid:str>/', professional_postback),
     path('api/', jwt_cookie(csrf_exempt(GraphQLView.as_view(graphiql=True)))),
-]
+ ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
