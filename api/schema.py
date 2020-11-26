fdc6fa675709346972b5e074e606306a0aafb1c3
guilhermewebdev <guilherme@guilhermeweb.dev>
2020-11-26 19:56:17 -0300

    melhoria nos nomes das mutations de auth

--- api/schema.py
+++ api/schema.py
@@ -1,11 +1,12 @@
+from django.contrib.auth import login, logout
 import graphene
 import graphql_jwt
 from core import schemas as core
 class Mutation(graphene.ObjectType, core.Mutation):
-    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    login = graphql_jwt.ObtainJSONWebToken.Field()
+    logout = graphql_jwt.DeleteJSONWebTokenCookie.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
-    delete_token_cookie = graphql_jwt.DeleteJSONWebTokenCookie.Field()
     delete_refresh_token_cookie = graphql_jwt.DeleteRefreshTokenCookie.Field()
 
 
