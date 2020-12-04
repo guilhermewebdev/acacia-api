9ac69877ef68bd2113865dd85a59ae3c94bca5d1
Guilherme Isaías <guilherme@localhost.localdomain>
2020-12-04 20:10:09 -0300

    Método para pegar queryset das propostas

--- services/views.py
+++ services/views.py
@@ -1,5 +1,13 @@
+from django.db.models.query_utils import Q
+from . import models
 from django.shortcuts import render
 from rest_framework.viewsets import ViewSet, ModelViewSet
 
 class ProposalsViewset(ModelViewSet):
-    pass
+    
+    def get_queryset(self):
+        if self.request.user.is_professional:
+            return models.Proposal.objects.filter(
+                Q(client=self.request.user) |
+                Q(professional=self.request.user.professional)
+            ).all()
